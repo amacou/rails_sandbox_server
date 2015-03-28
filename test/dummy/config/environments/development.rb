2e638fdff1d3fdbaa3633ca46c9f563ed9894ef4
amacou <amacou.abf@gmail.com>
2015-03-28 19:18:08 +0900

    change dummy project config

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.rails_sandbox_server.fixture_path = 'test/new/fixtures'
 end
