94a79e0492ba11b16e6c613dcf3c8163e4cff76a
amacou <amacou.abf@gmail.com>
2015-06-25 20:54:42 +0900

    rollback METHOD change DELETE to GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RailsSandboxServer::Engine.routes.draw do
-  delete 'rollback' => 'sandbox#rollback', as: :rollback
+  get 'rollback' => 'sandbox#rollback', as: :rollback
   post 'setup' => 'sandbox#setup', as: :setup
 end
